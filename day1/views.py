ea1e5b73d72bda26de47fb271a7ce56bac1dacae
taewoo-dev <twin9774@gmail.com>
2024-08-21 23:06:24 +0900

    ✨feat: chapter_01 과제 업로드

---
+++ day1/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from day1.fake_db import user_db
+
+
+# Create your views here.
+def user_list(request):
+    user = user_db
+    print(user)
+    context = {
+        "user_list": user
+    }
+    return render(request, "user_list.html", context)
+
+
+def user_info(request, user_id):
+    user = user_db[user_id]
+    context = {
+        'user': user
+    }
+    return render(request, "user_info.html", context)
